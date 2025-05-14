3984465ee1561b257680373ca151dca51469b8c6
MyungGyun Kim <kapliff89@gmail.com>
2025-05-14 12:10:30 +0900

    Prettier 설정 변경, 패키지 의존성 추가 및 수정, 새로운 구성 파일 추가, SVG 아이콘 추가, 기존 아이콘 삭제

---
+++ src/lib/config.ts
@@ -0,0 +1,9 @@
+export interface Config {
+    title: string;
+    description: string;
+}
+
+export const config: Config = {
+    title: 'Lilamaris',
+    description: 'Lilamaris is a platform for creating and sharing content.'
+};

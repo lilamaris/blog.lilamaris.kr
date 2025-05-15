b4dbdd1fcfe1e9780830cefc3b9960d573b6ae2d
MyungGyun Kim <kapliff89@gmail.com>
2025-05-16 08:57:16 +0900

    경로 별칭 추가: svelte.config.js에서 $src 및 $components 경로 별칭을 설정하여 코드 가독성 향상

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,8 @@ const config = {
         // See https://svelte.dev/docs/kit/adapters for more information about adapters.
         adapter: adapter(),
         alias: {
-            $src: '/src/'
+            $src: './src',
+            $components: './src/lib/components'
         }
     }
 };

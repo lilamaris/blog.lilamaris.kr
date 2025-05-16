246baed88c3d3f38745b331cf2c915e287798421
MyungGyun Kim <kapliff89@gmail.com>
2025-05-16 13:19:04 +0900

    게시글 요약 컴포넌트(PostAbstract) 추가 및 게시글 페이지에서의 사용으로 코드 구조 개선

---
+++ src/lib/components/layout/write/PostAbstract.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { formatDate } from '$lib/utils';
+    import CategoryList from '$lib/components/layout/CategoryList.svelte';
+    import { Route } from '$lib/config';
+
+    interface Props {
+        post: Post;
+    }
+
+    const { post }: Props = $props();
+</script>
+
+<section class="flex flex-col gap-1">
+    <h1 class="text-flg font-bold">{post.title}</h1>
+    <h2 class="text-base-content/80">{post.description}</h2>
+    <div class="flex gap-3">
+        <small class="text-base-content/60">작성: {formatDate(post.createdAt)}</small>
+        <small class="text-base-content/60">수정: {formatDate(post.updatedAt)}</small>
+    </div>
+    <CategoryList
+            class="mt-4 flex gap-1"
+            categories={post.categories}
+            queryOption={{ prefix: Route.Write }}
+        />
+</section>
--- src/routes/write/[slug]/+page.svelte
+++ src/routes/write/[slug]/+page.svelte
@@ -1,26 +1,12 @@
 <script lang="ts">
-    import { formatDate } from '$lib/utils';
-    import CategoryList from '$lib/components/layout/CategoryList.svelte';
-    import { Route } from '$lib/config';
+    import PostAbstract from '$lib/components/layout/write/PostAbstract.svelte';
     const { data } = $props();
-    const post = data.post;
+    const { post, html } = data;
 </script>
 
-<div class="max-w-content mx-auto">
-    <div class="flex flex-col">
-        <h1 class="text-flg font-bold">{post.title}</h1>
-        <h2 class="text-base-content/80">{post.description}</h2>
-        <div class="mt-4 flex gap-3">
-            <small class="text-base-content/60">작성: {formatDate(post.createdAt)}</small>
-            <small class="text-base-content/60">수정: {formatDate(post.updatedAt)}</small>
-        </div>
-    </div>
-    <CategoryList
-        class="mt-4 flex gap-1"
-        categories={post.categories}
-        queryOption={{ prefix: Route.Write }}
-    />
-    <div class="prose mx-2">{@html data.html}</div>
+<div class="max-w-content mx-auto flex flex-col gap-4">
+    <PostAbstract {post} />
+    <div class="prose">{@html html}</div>
     <div class="text-base-content/60 my-4 flex justify-start text-xs">
         <span>by {post.author?.name ?? '익명'}</span>
     </div>

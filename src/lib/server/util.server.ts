8fd7881e0a0d4e63f382f742e3c6ff050bd04ce6
MyungGyun Kim <kapliff89@gmail.com>
2025-05-20 21:51:08 +0900

    환경에 따라 API 기본 URL 설정 방식 개선: 프로덕션 및 개발 환경에 맞춰 API 기본 URL을 간소화하여 코드 가독성을 향상시킴.

--- src/lib/server/util.server.ts
+++ src/lib/server/util.server.ts
@@ -1,9 +1,7 @@
 import { env } from '$env/dynamic/private';
 
 const baseUrl =
-    process.env.NODE_ENV === 'production'
-        ? `https://${env.PROD_API_BASE_URL}`
-        : `http://${env.DEV_API_BASE_URL}/api`;
+    process.env.NODE_ENV === 'production' ? env.PROD_API_BASE_URL : env.DEV_API_BASE_URL;
 
 export const getBaseUrl = () => {
     return baseUrl;

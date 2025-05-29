1b4005b5669a9721cec653d7d0d97d7c427478c3
MyungGyun Kim <kapliff89@gmail.com>
2025-05-30 01:35:26 +0900

    remove: unused function

--- src/lib/utils/form.ts
+++ src/lib/utils/form.ts
@@ -1,8 +1,3 @@
-// export const formatDate = (date: Date, locales = 'ko-KR') => {
-//     const dateFormatter = new Intl.DateTimeFormat(locales);
-//     return dateFormatter.format(date);
-// };
-
 export const formatDate = (date: Date | string, locales = 'ko-KR') => {
     const d = new Date(date);
     return d.toLocaleString(locales);

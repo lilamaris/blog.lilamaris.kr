e75f538603274099ff735628991b8e26e53246c9
lilamaris.dev@gmail.com <kapliff89@gmail.com>
2025-06-05 05:22:11 +0900

    스타일 수정: clamp text size 제거

--- src/lib/components/write/PostAbstract.svelte
+++ src/lib/components/write/PostAbstract.svelte
@@ -13,10 +13,10 @@
 
 <section class="flex flex-col gap-1">
     <h1 class="text-xl font-bold">{post.title}</h1>
-    <h2 class="text-base-content/80">{post.description}</h2>
+    <p class="text-base-content/80 text-sm">{post.description}</p>
     <div class="flex gap-3">
-        <small class="text-base-content/60">작성: {formatDate(post.createdAt)}</small>
-        <small class="text-base-content/60">수정: {formatDate(post.updatedAt)}</small>
+        <small class="text-base-content/40">작성: {formatDate(post.createdAt)}</small>
+        <small class="text-base-content/40">수정: {formatDate(post.updatedAt)}</small>
     </div>
     <CategoryList
         class="mt-4 flex gap-1"

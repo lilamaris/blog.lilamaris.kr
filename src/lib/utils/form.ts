2994c1f16fc308e65dce7428dceaaac8bb8ad6a5
lilamaris.dev@gmail.com <kapliff89@gmail.com>
2025-06-05 05:22:22 +0900

    날짜 형식 개선: formatDate 함수에서 날짜를 '년-월-일' 형식으로 포맷

--- src/lib/utils/form.ts
+++ src/lib/utils/form.ts
@@ -1,4 +1,10 @@
 export const formatDate = (date: Date | string, locales = 'ko-KR') => {
     const d = new Date(date);
-    return d.toLocaleString(locales);
+    return d
+        .toLocaleDateString(locales, {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit'
+        })
+        .replace(/\./g, '.');
 };

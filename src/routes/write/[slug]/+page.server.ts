1e61a2b82b81cefa51711e52998c318d9bbaaf6a
lilamaris.dev@gmail.com <kapliff89@gmail.com>
2025-06-05 10:00:34 +0900

    기능 추가: 게시글 로드 시 HTML과 함께 목차(toc) 반환 및 Svelte 컴포넌트에서 목차 표시 기능 추가

--- src/routes/write/[slug]/+page.server.ts
+++ src/routes/write/[slug]/+page.server.ts
@@ -3,7 +3,7 @@ import { markdownToHtml } from '$lib/markdown';
 import type { PageServerLoad } from './$types';
 import type { Post } from '$lib/types/post';
 import { getPostBySlug } from '$lib/server/post.server';
-
+import type { TocItem } from '$lib/markdown/plugins';
 /**
  * slug로 단일 게시글을 조회하고, 마크다운을 HTML로 변환합니다.
  * @param params - URL 파라미터(slug)
@@ -14,12 +14,13 @@ export const load: PageServerLoad = async ({
 }): Promise<{
     post: Post;
     html: string;
+    toc: TocItem[];
 }> => {
     try {
         const post = await getPostBySlug(params.slug);
         if (!post) throw new Error('포스트를 찾을 수 없습니다.');
-        const html = await markdownToHtml(post.content);
-        return { post, html };
+        const { html, toc } = await markdownToHtml(post.content);
+        return { post, html, toc };
     } catch (err) {
         console.error(err);
         error(404, '포스트를 찾을 수 없습니다.');
--- src/routes/write/[slug]/+page.svelte
+++ src/routes/write/[slug]/+page.svelte
@@ -1,16 +1,27 @@
 <script lang="ts">
     import PostAbstract from '$lib/components/write/PostAbstract.svelte';
     import SEO from '$lib/components/SEO.svelte';
-    
+
     const { data } = $props();
-    const { post, html } = data;
+    const { post, html, toc } = data;
 </script>
 
 <SEO title={post.title} description={post.description} />
 
 <div class="max-w-content mx-1 flex flex-col gap-4 md:mx-auto">
     <PostAbstract {post} />
-    <div class="prose">{@html html}</div>
+    <article class="prose max-w-content relative">
+        <div class="absolute left-full">
+            <ul class="menu fixed w-max">
+                {#each toc as item}
+                    <li>
+                        <a href={`#${item.id}`}>{item.text}</a>
+                    </li>
+                {/each}
+            </ul>
+        </div>
+        {@html html}
+    </article>
     <div class="text-base-content/60 my-4 flex justify-start text-xs">
         <span>by {post.author?.name ?? '익명'}</span>
     </div>
